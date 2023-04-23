7be93282b0952c622e90f779893359ab0a60e74c
nchng1 <411428340@qq.com>
2023-04-23 21:18:59 +0800

    引入springboot

---
+++ service/src/main/java/com/nchng1/App.java
@@ -0,0 +1,13 @@
+package com.nchng1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web/src/main/java/com/nchng1/web/TestController.java
@@ -0,0 +1,13 @@
+package com.nchng1.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    private String test() {
+        return "the world!!";
+    }
+}
