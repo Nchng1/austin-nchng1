3835951b204402038a1bd44e6784231e46898e69
nchng1 <411428340@qq.com>
2023-04-25 00:46:01 +0800

    2.接入 lombok、guava、hutool、fastjson

--- web/src/main/java/com/nchng1/AustinApplication.java
+++ web/src/main/java/com/nchng1/AustinApplication.java
@@ -1,12 +1,26 @@
 package com.nchng1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AustinApplication {
-
+    private final Logger logger = LoggerFactory.getLogger(AustinApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(AustinApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        logger.error("error logback for austin");
+        logger.info("info logback for austin");
+        return String.format("Hello %s!", name);
     }
 }
